70d48bd9044e9fa0d51571bf95384ecfdebc6adf
Jayesh korat <jayeshkorat18@gmail.com>
2018-06-06 13:09:29 +0530

    updates to deploy to heroku

--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { FieldErrorDisplayModule } from './shared/helper/field-error-display/field-error-display.module';
 import { ModalModule } from 'ngx-bootstrap';
-import { Ng2SmartTableModule } from 'ng2-smart-table';
+//import { Ng2SmartTableModule } from 'ng2-smart-table';
 
 
 @NgModule({
   exports: [
     FieldErrorDisplayModule,
     ModalModule,
-    Ng2SmartTableModule
+    //Ng2SmartTableModule
   ],
   imports:[ModalModule.forRoot()]
 })
